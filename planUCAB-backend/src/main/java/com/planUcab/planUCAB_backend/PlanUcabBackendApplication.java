7f067cc109be78ed166c5edc7450dbe988b1f9d7
Devony <duodevo@gmail.com>
2025-09-23 20:36:13 -0300

    [+] Creacion del backend

---
+++ planUCAB-backend/src/main/java/com/planUcab/planUCAB_backend/PlanUcabBackendApplication.java
@@ -0,0 +1,13 @@
+package com.planUcab.planUCAB_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanUcabBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanUcabBackendApplication.class, args);
+	}
+
+}
---
+++ planUCAB-backend/src/test/java/com/planUcab/planUCAB_backend/PlanUcabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planUcab.planUCAB_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanUcabBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
