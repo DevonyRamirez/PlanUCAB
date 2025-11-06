ae8325b6b1a459ae5d0f163195022bfa46b2447b
diegoprietoucab <daprieto.24@est.ucab.edu.ve>
2025-11-06 19:42:25 -0400

    (+) organización de componentes corregida

--- planUCAB-backend/src/main/java/com/planUcab/planUCAB_backend/exceptions/InvalidEventTimeException.java
+++
@@ -1,9 +0,0 @@
-package com.planUcab.planUCAB_backend.exceptions;
-
-public class InvalidEventTimeException extends EventException {
-
-    public InvalidEventTimeException(String message) {
-        super(message);
-    }
-}
-
